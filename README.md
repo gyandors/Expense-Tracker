4fd587176fce08d645ef2545aee765d89907bc70
Sachin Gyandor <gyandors@gmail.com>
2024-09-06 14:01:33 +0530

    Missed files during previous commit

--- README.md
+++ README.md
@@ -1,8 +1,12 @@
-# Expense tracker web app with local storage integration
+# Expense tracker web app
 
 <br>
 This project was build using HTM, JavaScript and Bootstrap.
 <br>
-We can add the expenses and can be modify and can be deleted.
+We can track our expenses and it has the features for modifying and deleting the expense.
 <br>
-It uses the browser local storage to store the data.
+Using Node and Express for building server.
+<br>
+Using MySQL database and the package 'mysql2' driver for Node.js.
+<br>
+Using Sequelize for making SQL queries ease.
--- index.js
+++ index.js
@@ -1,70 +1,103 @@
-const form = document.getElementById('add-expense');
+const form = document.getElementById("add-expense");
+let editing = false,
+  editingId = null;
 
-form.addEventListener('submit', function (event) {
+form.addEventListener("submit", function (event) {
   event.preventDefault();
 
-  //Grabing the values from input fields
-  const amount = document.getElementById('amount').value;
-  const desc = document.getElementById('desc').value;
-  const category = document.getElementById('category').value;
+  const amount = document.getElementById("amount").value;
+  const category = document.getElementById("category").value;
+  const description = document.getElementById("desc").value;
 
-  //Creating object from input field values
-  const date = new Date();
-  const keyForobj = Number(
-    `${date.getDate()}${date.getHours()}${date.getMinutes()}${date.getSeconds()}`
-  );
-  const obj = { keyForobj, amount, category, desc };
-  localStorage.setItem(keyForobj, JSON.stringify(obj));
+  const obj = { amount, category, description };
 
-  displayExpences(obj);
-
-  //Clearing the input fields
-  document.getElementById('amount').value = '';
-  document.getElementById('desc').value = '';
-  document.getElementById('category').value = '';
+  if (!editing) {
+    fetch("http://localhost:3000/api/expenses", {
+      method: "POST",
+      body: JSON.stringify(obj),
+      headers: {
+        "content-type": "application/json",
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        displayExpences(data);
+        document.getElementById("amount").value = "";
+        document.getElementById("desc").value = "";
+        document.getElementById("category").value = "";
+      })
+      .catch((err) => console.log(err));
+  } else {
+    fetch(`http://localhost:3000/api/expenses/${editingId}`, {
+      method: "PUT",
+      body: JSON.stringify(obj),
+      headers: {
+        "content-type": "application/json",
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        displayExpences({ ...obj, _id: editingId });
+        document.getElementById("amount").value = "";
+        document.getElementById("desc").value = "";
+        document.getElementById("category").value = "";
+        editing = false;
+        editingId = null;
+      })
+      .catch((err) => console.log(err));
+  }
 });
 
-//Displaying the details stored in local storage on window load
-window.addEventListener('DOMContentLoaded', function () {
-  Object.keys(this.localStorage).forEach((val) => {
-    displayExpences(JSON.parse(this.localStorage.getItem(val)));
-  });
+window.addEventListener("DOMContentLoaded", function () {
+  fetch("http://localhost:3000/api/expenses")
+    .then((res) => res.json())
+    .then((data) => {
+      for (const val of data) displayExpences(val);
+    })
+    .catch((err) => console.log(err));
 });
 
 function displayExpences(obj) {
-  const list = document.createElement('li');
-  list.className = ' list-group-item list-group-item-dark';
-  list.innerHTML = `Rs. ${obj.amount} - ${obj.category}<p style="font-style:italic">${obj.desc}</p>`;
+  const list = document.createElement("li");
+  list.className = " list-group-item list-group-item-dark";
+  list.innerHTML = `Rs. ${obj.amount} - ${obj.category}<p style="font-style:italic">${obj.description}</p>`;
 
-  const delBtn = document.createElement('button');
-  delBtn.innerHTML = 'Delete';
-  delBtn.className = 'btn btn-danger btn-sm';
+  const delBtn = document.createElement("button");
+  delBtn.innerHTML = "Delete";
+  delBtn.className = "btn btn-danger btn-sm";
   delBtn.setAttribute(
-    'style',
-    'padding-y: .25rem; padding-x: .5rem; font-size: .6rem;'
+    "style",
+    "padding-y: .25rem; padding-x: .5rem; font-size: .6rem;"
   );
-  delBtn.addEventListener('click', function () {
-    records.removeChild(delBtn.parentElement);
-    localStorage.removeItem(obj.keyForobj);
+  delBtn.addEventListener("click", function () {
+    fetch(`http://localhost:3000/api/expenses/${obj._id}`, {
+      method: "DELETE",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        records.removeChild(delBtn.parentElement);
+      })
+      .catch((err) => console.log(err));
   });
   list.appendChild(delBtn);
 
-  const editBtn = document.createElement('button');
-  editBtn.innerHTML = 'Edit';
-  editBtn.className = 'btn btn-danger btn-sm';
+  const editBtn = document.createElement("button");
+  editBtn.innerHTML = "Edit";
+  editBtn.className = "btn btn-danger btn-sm";
   editBtn.setAttribute(
-    'style',
-    'padding-y: .25rem; padding-x: .5rem; font-size: .6rem; margin-left:10px'
+    "style",
+    "padding-y: .25rem; padding-x: .5rem; font-size: .6rem; margin-left:10px"
   );
-  editBtn.addEventListener('click', function () {
-    document.getElementById('amount').value = obj.amount;
-    document.getElementById('desc').value = obj.desc;
-    document.getElementById('category').value = obj.category;
-    records.removeChild(delBtn.parentElement);
-    localStorage.removeItem(obj.keyForobj);
+  editBtn.addEventListener("click", function () {
+    editing = true;
+    editingId = obj._id;
+    document.getElementById("amount").value = obj.amount;
+    document.getElementById("desc").value = obj.description;
+    document.getElementById("category").value = obj.category;
+    records.removeChild(editBtn.parentElement);
   });
   list.appendChild(editBtn);
 
-  const records = document.getElementById('records');
+  const records = document.getElementById("records");
   records.appendChild(list);
 }
